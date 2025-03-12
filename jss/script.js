df64fb36a5397b3765dd9b77dd1bf56608eacdcf
dzgeverywhere <ice53669@gmail.com>
2025-03-11 21:34:45 -0300

    Create script.js

---
+++ jss/script.js
@@ -0,0 +1,8 @@
+console.log("%c the little ear - i didn't want this game to play", "background: #261d24;color:#f2e8df;padding:5px;border-radius: 5px;line-height: 26px; font-size:20px");
+
+console.log("%cPRODUCED BY:", "font-weight:bold; font-size:14px; padding-left:8px; margin-top:20px;");
+console.log("%c the little ear and aimer", "font-size: 12px; line-height:26px");
+
+// BOLD
+console.log("%cHTML BY:", "font-weight:bold; font-size:14px; padding-left:8px; margin-top:20px;");
+console.log("%c aimer", "font-size: 12px; line-height:26px");
